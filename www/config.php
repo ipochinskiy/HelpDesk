bd39ca2bac1ee969a04ed6264629dbb12af82be3
ipochinskiy <zorkiypnz@yandex.ru>
2012-09-11 03:36:50 +0400

    + some definitions

--- www/config.php
+++ www/config.php
@@ -4,6 +4,7 @@ defined("PORTAL_ROOT_PATH") || define("PORTAL_ROOT_PATH", __DIR__ . "/");
 
 defined("CSS_PATH") || define("CSS_PATH", PORTAL_ROOT_PATH . "css/");
 defined("JS_PATH") || define("JS_PATH", PORTAL_ROOT_PATH . "js/");
+defined("LIB_PATH") || define("LIB_PATH", PORTAL_ROOT_PATH . "lib/");
 
 defined("PORTAL_ROUTING_PATH") || define("PORTAL_ROUTING_PATH", PORTAL_ROOT_PATH . "app/routing.php");
 defined("CONTROLLERS_PATH") || define("CONTROLLERS_PATH", PORTAL_ROOT_PATH . "app/controllers/");
@@ -17,7 +18,7 @@ defined("ETC_PATH") || define("ETC_PATH", FILES_PATH . "etc/");
 defined("IMG_PATH") || define("IMG_PATH", FILES_PATH . "img/");
 
 defined("DEBUG_MODE") || define("DEBUG_MODE", 1);
-//defined("USED_CSS") || define("USED_CSS", array());
-//defined("USED_JS") || define("USED_JS", array());
+defined("USED_CSS") || define("USED_CSS", array());
+defined("USED_JS") || define("USED_JS", array());
 
 ?>
