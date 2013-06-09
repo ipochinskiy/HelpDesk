74fdf23c285a096f1060629c7d3f7a76ce21955b
Alexpts <alexpts@yandex.ru>
2013-06-09 19:24:27 +0400

    fix modal

--- www/app/views/news.php
+++ www/app/views/news.php
@@ -1,16 +1,16 @@
-<!--<div id="dialog-form" title="Create new user">-->
-<!--    <p class="validateTips">All form fields are required.</p>-->
-<!--    <form>-->
-<!--        <fieldset>-->
-<!--            <label for="name">Name</label>-->
-<!--            <input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />-->
-<!--            <label for="email">Email</label>-->
-<!--            <input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />-->
-<!--            <label for="password">Password</label>-->
-<!--            <input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />-->
-<!--        </fieldset>-->
-<!--    </form>-->
-<!--</div>-->
+<div id="dialog-form" title="Create new user">
+    <p class="validateTips">All form fields are required.</p>
+    <form action="/" method="post">
+        <fieldset>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
+        </fieldset>
+    </form>
+</div>
 
 <div id="news">
     <div id="news-add">
